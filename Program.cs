d6952c9b4de49b8ceb149cb60b3862e9c3610bf4
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-28 19:23:36 +0530

    Added: initial push

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Name");
+            string name = Console.ReadLine();
+            Random random = new Random();
+            int isPresent = random.Next(0,2);
+            if (isPresent == 0)
+            {
+                Console.WriteLine(name + "Absent");
+            }
+            else 
+            {
+                Console.WriteLine(name + "Present");
+
+            }
+
+
+        }
+    }
+}
