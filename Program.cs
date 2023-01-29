5e89e3ede49b354cc47f010dcaa9f3b1a481aaab
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-29 07:34:24 +0530

    Added: Using Switch case

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullday = 8;
+            int halfday = 4;
+            int perWages = 20;
+            Random random = new Random();
+            int number = random.Next(0, 3);
+
+
+            switch (number)
+            {
+                case 0:
+                Console.WriteLine("Employee Hour is 8");
+                Console.WriteLine("Wage is" + perWages * fullday);
+                break;
+
+                case 1:
+                Console.WriteLine("Employee Hour is 4");
+                Console.WriteLine("Wage is" + perWages * halfday);
+                break;
+
+                default:
+                Console.WriteLine("Employee Hour is 0");
+                break;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
